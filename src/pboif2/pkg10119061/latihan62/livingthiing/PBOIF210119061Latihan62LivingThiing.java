a6a207002e6a0f7e429cdfd146d5e2269b5e2536
ejaakml <eximindotong@yahoo.com>
2020-11-29 12:18:24 +0700

    62

---
+++ src/pboif2/pkg10119061/latihan62/livingthiing/PBOIF210119061Latihan62LivingThiing.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan62.livingthiing;
+
+/**
+ *
+ * @author Rheiza
+ */
+public class PBOIF210119061Latihan62LivingThiing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                Human h = new Human();
+        h.setNama("Rizki Adam Kurniawan");
+        h.walk(h.getNama());
+        h.breath(h.getNama());
+        h.eat(h.getNama());
+    }
+    }
+    
+
